fa4145c6f9aba02b88629304380859bc9fadefdd
webscript1 <santana_daniel7@hotmail.com>
2024-04-12 18:28:17 -0400

    update

--- src/app/services/Global.ts
+++ src/app/services/Global.ts
@@ -1,7 +1,7 @@
 export const Global={
 
-  url:'https://elscriptfarmeador.onrender.com/',
- //url:`http://localHost:3001/`,
+  //url:'https://elscriptfarmeador.onrender.com/',
+ url:`http://localHost:3001/`,
   url_version:'v1',
   token: (): string | null=> {
         if (typeof window !== 'undefined') {
